235790d53ad8fef206545082bed68584c671fca3
Dirk Einecke <dirk.einecke@gmail.com>
2019-02-16 17:31:34 +0100

    Check response code exists added

--- src/main.rs
+++ src/main.rs
@@ -44,28 +44,56 @@ fn main() {
 
             let json: Value = serde_json::from_str(&response_body).unwrap();
 
-            if json["success"].is_boolean() {
-                let s = json["success"].as_bool().unwrap();
-                if s {
-                    println!("Success");
-                } else {
-                    println!("Oops, something went wrong.");
+            match json.get("success") { // Key "success" exists?
+                Some(_) => {
+                    if json["success"].is_boolean() {
+                        let s = json["success"].as_bool().unwrap();
+                        if s {
+                            println!("Success");
+                        } else {
+                            println!("Oops, something went wrong.");
 
-                    if json["errors"].is_array() {
-                        let errors = json["errors"].as_array().unwrap();
+                            match json.get("errors") { // Key "errors" exists?
+                                Some(_) => {
+                                    if json["errors"].is_array() {
+                                        let errors = json["errors"].as_array().unwrap();
 
-                        for error in errors {
-                            if error.is_object() {
-                                let error = error.as_object().unwrap();
-                                if error["message"].is_string() && error["code"].is_number() {
-                                    println!("  -> {} ({})", error["message"].as_str().unwrap(), error["code"]);
+                                        for error in errors {
+                                            if error.is_object() {
+                                                let error = error.as_object().unwrap();
+                                                match error.get("message") { // Key "message" exists?
+                                                    Some(_) => {
+                                                        match error.get("code") { // Key "code" exists?
+                                                            Some(_) => {
+                                                                if error["message"].is_string() && error["code"].is_number() {
+                                                                    println!("  -> {} ({})", error["message"].as_str().unwrap(), error["code"]);
+                                                                }
+                                                            },
+                                                            None => {
+                                                                // Key "code" not found
+                                                            },
+                                                        }
+                                                    },
+                                                    None => {
+                                                        // Key "message" not found
+                                                    },
+                                                }
+                                            }
+                                        }
+                                    }
+                                },
+                                None => {
+                                    println!("Oops, something went wrong."); // Key "errors" not found
                                 }
                             }
                         }
+                    } else {
+                        println!("Oops, something went wrong."); // Key "success" is not boolean
                     }
-                }
-            } else {
-                println!("Oops, something went wrong.");
+                },
+                None => {
+                    println!("Oops, something went wrong."); // Key "success" not found
+                },
             }
         }
     }
